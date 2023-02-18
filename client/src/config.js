90b23975cde66864085d6efa7a2ee17f169abea3
Grzegorz Woźniak <grzegorz.wozniak1989@gmail.com>
2023-02-18 17:50:37 +0100

    changes

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? '/' : "http://localhost:8001";
+export const API_URL = process.env.NODE_ENV === 'production' ? '' : "http://localhost:8001";
